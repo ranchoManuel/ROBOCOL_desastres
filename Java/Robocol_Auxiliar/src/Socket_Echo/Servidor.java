dec5b7b22f957c3804b8fdc4ef988d371b9e00de
Juan Manuel <jmanuel816@gmail.com>
2015-04-10 15:09:30 -0500

    Hello World Comunicacion Serial y hello world sockets

--- Java/Robocol_Auxiliar/src/Socket_Echo/Servidor.java
+++ Java/Robocol_Auxiliar/src/Socket_Echo/Servidor.java
@@ -1,4 +1,4 @@
-package serverSocket_Juguete;
+package Socket_Echo;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,13 +12,10 @@ public class Servidor
 
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Hola 1");
 		ServerSocket serverSocket = new ServerSocket(PUERTO);
 		Socket clientSocket = serverSocket.accept();
-		System.out.println("Lo llamaron");
 		PrintWriter out=new PrintWriter(clientSocket.getOutputStream(), true);
 		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		System.out.println("Hola 2");
 
 		// Initiate conversation with client
         for(String inputLine; (inputLine = in.readLine()) != null;)
