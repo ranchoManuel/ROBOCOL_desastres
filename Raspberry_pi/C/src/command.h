0f1d968a6c26f128cca17bb0413044b8268c194c
Juan Manuel <jmanuel816@gmail.com>
2015-06-24 17:16:10 -0500

    Nueva carpeta Raspberry pi

---
+++ Raspberry_pi/C/src/command.h
@@ -0,0 +1,11 @@
+typedef struct command {
+    char* name;
+    char* arg1;
+    char* arg2;
+} command;
+
+command* init_command(char*, char*, char*);
+int get_priority(command*);
+void display_command(command*);
+void destroy_command(command*);
+char* command_string(command*);
