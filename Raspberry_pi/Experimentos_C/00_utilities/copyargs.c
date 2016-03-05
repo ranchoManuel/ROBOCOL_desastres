b224e44a86acf24000731dfa8ff80bcb811fc5b5
scvalencia <sc.valencia606@uniandes.edu.co>
2016-03-04 19:07:50 -0500

    Experiments and so

---
+++ Raspberry_pi/Experimentos_C/00_utilities/copyargs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PADDING 10000000
+
+void print_array();
+
+int length;
+char** cpy;
+
+int main (int argc, char *argv[]) {
+ 	cpy = malloc(argc * PADDING * sizeof(char));
+	length = argc;
+
+	int i = 0;
+	for (; i < length; i++) {
+    	*(cpy + i) = malloc(sizeof(char) * (strlen(*(argv + i))));
+    	strcpy(*(cpy + i), *(argv + i));
+	}
+
+	print_array();	
+	return 0;
+ }
+
+void print_array() {
+	int i = 0;
+	for(; i < length; i++)
+		printf("%s\n", *(cpy + i));
+}
